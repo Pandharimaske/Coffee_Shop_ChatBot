d62ec9b619561d75cd6b9dbdc5fb53e30d60110e
Pandhari <pandhari2527@gmail.com>
2025-08-02 17:07:27 +0530

    utils upd

--- Python_Backend/Backend/utils/util.py
+++ Python_Backend/Backend/utils/util.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-# from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
+_llm_cache = {}
 
-def load_llm(temperature:float = 0):
-    # return ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=temperature, api_key=os.getenv("GEMINI_API_KEY"))
-    return ChatGroq(model=os.getenv("GROQ_MODEL_NAME"), temperature=temperature)
+def load_llm(temperature: float = 0.0):
+    if temperature not in _llm_cache:
+        _llm_cache[temperature] = ChatGroq(model=os.getenv("GROQ_MODEL_NAME"), temperature=temperature)
+    return _llm_cache[temperature]
+
